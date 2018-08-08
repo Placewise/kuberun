e84b502d213678576f3436ce9bdfbde7168fbf12
kruczjak <kruczjak@gmail.com>
2018-08-08 09:26:47 +0200

    Fix initial specs to make travis pass

--- spec/integration/start_spec.rb
+++ spec/integration/start_spec.rb
@@ -3,12 +3,23 @@ RSpec.describe "`kuberun start` command", type: :cli do
     output = `kuberun help start`
     expected_output = <<-OUT
 Usage:
-  kuberun start DEPLOYMENT_NAME
+  kuberun start DEPLOYMENT-NAME
 
 Options:
-  -h, [--help], [--no-help]  # Display usage information
+  -h, [--help], [--no-help]                                  # Display usage information
+      [--certificate-authority=CERTIFICATE-AUTHORITY]        # See kubectl options
+      [--client-certificate=CLIENT-CERTIFICATE]              # See kubectl options
+      [--client-key=CLIENT-KEY]                              # See kubectl options
+      [--cluster=CLUSTER]                                    # See kubectl options
+      [--context=CONTEXT]                                    # See kubectl options
+      [--insecure-skip-tls-verify=INSECURE-SKIP-TLS-VERIFY]  # See kubectl options
+      [--kubeconfig=KUBECONFIG]                              # See kubectl options
+  -n, [--namespace=NAMESPACE]                                # See kubectl options
+      [--token=TOKEN]                                        # See kubectl options
+      [-v=N]                                                 # Log level, also passed to kubectl
+                                                             # Default: 0
 
-Command description...
+Starts pod for command
     OUT
 
     expect(output).to eq(expected_output)
--- spec/kuberun_spec.rb
+++ spec/kuberun_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Kuberun do
   it "has a version number" do
     expect(Kuberun::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
